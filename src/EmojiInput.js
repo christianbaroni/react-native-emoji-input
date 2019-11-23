9c78833e4e74e307829a11bc9cf7bd64e211b4ed
Christian Baroni <c@baroni.co>
2019-11-22 23:11:19 -0500

    Undo module:lodash

--- src/EmojiInput.js
+++ src/EmojiInput.js
@@ -15,7 +15,7 @@ import {
     LayoutProvider
 } from 'recyclerlistview';
 import Triangle from 'react-native-triangle';
-import _ from 'module:lodash';
+import _ from 'lodash';
 import {
     responsiveFontSize
 } from 'react-native-responsive-dimensions';
